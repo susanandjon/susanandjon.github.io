2c4e49af71211dd79b9f23624f414018c5d0fc39
susanandjon <43308055+susanandjon@users.noreply.github.com>
2018-12-16 13:35:56 -0500

    v0.1

--- old/js/main.js
+++
@@ -1,8 +0,0 @@
-// Animate Smooth Scroll
-$('#view-welcome').on('click', function() {
-    const intro = $('#intro').position().top;
-
-    $('html, body').animate({
-        scrollTop: intro
-    }, 900);
-});
